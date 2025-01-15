5fc858c64456d01b606236df7686748a02c42545
Erjon Ahmetaj <EA82650N@pace.edu>
2025-01-15 01:04:44 -0500

    Added form to contact me

--- minimal-portfolio/src/data/portfolio.js
+++ minimal-portfolio/src/data/portfolio.js
@@ -1,20 +1,20 @@
 export default [
     {
        title: 'Video Processing Service',
-       imgUrl: '/assets/terminal-website.jpg',
+       imgUrl: '???????????',
        stack: ['Next.js', 'CSS', 'JavaScript', "Fire Base", "Google Cloud Services"],
-       link: 'https://fkcodes.com',
+       link: '????????????',
     },
     {
        title: 'Developer Portfolio',
-       imgUrl: '/assets/dev-portfolio.jpg',
+       imgUrl: '??????????',
        stack: ['React', 'Tailwind', 'Vite'],
-       link: 'https://thehackershideout.com',
+       link: '???????????',
     },
     {
-       title: 'AI Nerf Gun',
-       imgUrl: '/assets/ai-nerf-gun.jpg',
-       stack: ['Python', 'C++', 'Robotics'],
-       link: 'https://youtu.be/A8eegxL82PI',
+       title: '??????',
+       imgUrl: '??????',
+       stack: ['???????'],
+       link: '??????????',
     },
  ]
